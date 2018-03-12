c244c6e4944abc69c013c39d5c06273c2d66e45a
root <root@savage>
2018-03-12 09:37:20 +0000

    first tests

---
+++ stop.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+status = 0
+
+if status == 0
+    os.system("TASKKILL /F /IM EcoServer.exe")
+    status = 1
+    os.system("TASKKILL /F /IM EcoServer.exe")
+    
+    else:
+        sys.exit()
+    
